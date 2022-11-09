8e006b2b420d2327a84f0b2f16be979518c7c72b
blackadder4 <55015249+blackadder4@users.noreply.github.com>
2022-11-08 21:37:12 -0800

    Rewrittened the qr code gen with qrious lib
    Also introduced the hash to the qr code

--- experimental_QR_code_Gen.js
+++ experimental_QR_code_Gen.js
@@ -1,66 +1,24 @@
-// this is a further dev of the stable version on the main branch uploaded by me. In this one I am trying to get a aysnc await server to client system working
-let QR;
-let UID;
-let type;
-
-const QR_button = document.querySelector("#QR_Button");
-
-// add the server connection , to obtain QR code using async awaits
-function Request_QR() {
-  fetch("/QR", {
-     
-    // Adding method type
-    method: "POST",
-     
-    // Adding body or contents to send
-    body: JSON.stringify({
-        title: "Request_QR",
-        body: "Type : " + type,
-        userId: UID
-    }),
-     
-    // Adding headers to the request
-    headers: {
-        "Content-type": "application/json; charset=UTF-8"
-    }
-})
- 
-// Converting to JSON
-.then(response => response.json())
- 
-// Displaying results to console
-.then(json => console.log(json));
-  
-
-  intervalId = setInterval(wait_time, 800);
-}
-
-
-// potentially used by the server side
-function Generate_QR_code(){
-    //new var that is of type qr code
-    var qrcode = new QRCode("qrcode");
     //function that makes the code
-    function makeCode () {
+    function makeCode (user_name, presentation_name, session_id) {
         // sub function that makes randomNumbers
-        function randomNumber(len) {
-        var randomNumber;
-        var n = '';
-        // for loop in range of count
-        for(var count = 0; count < len; count++) {
-        //floor the random number and times 10
-            randomNumber = Math.floor(Math.random() * 10);
-            //concatenate the strings to get the final random number
-            n += randomNumber.toString();
-        }
-        //return the random number
-        return n;
-    }
-    // gives the length of random number here
-    var value = randomNumber(// in put the length here);
-        var elText = value;
-        // this part generates the actual qr code
-        qrcode.makeCode(elText);
-    }
-    makeCode();
+        let result = user_name.concat(presentation_name, session_id);
+        // master hash to go off from
+        // this is a sha 256 function from github
+        async function sha256(message) {
+          // encode as UTF-8
+          const msgBuffer = new TextEncoder().encode(message);                    
+          // hash the message
+          const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+          // convert ArrayBuffer to Array
+          const hashArray = Array.from(new Uint8Array(hashBuffer));
+          // convert bytes to hex string                  
+          const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+          return hashHex;
+          }
+          var seed = sha256(result);
+          new QRious({
+            element: qrcodeContainer,
+            value: seed
+          });
+      }
 });
